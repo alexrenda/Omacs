b6b65d9a4da3f0277dbec3a7eeb45c6737f87e0d
Zander Bolgar <lezed1@gmail.com>
2015-11-01 01:33:13 -0500

    First try at some buffers!

---
+++ src/obuffer.ml
@@ -0,0 +1,52 @@
+module type OBuffer = sig
+  type buf
+  type cur
+
+  val make : unit -> buf
+  
+  (* General getters *)
+  val get_text : buf -> string
+  val get_cursor : buf -> cur
+
+  (* Basic cursor interaction *)
+  val set_cursor : buf -> cur -> buf
+  val move_cursor_right : buf -> buf
+  val move_cursor_left : buf -> buf
+
+  (* Basic text interaction *)
+  val set_text : buf -> string -> buf
+  val insert_char : buf -> char -> buf
+  val delete_char : buf -> buf
+  
+end
+
+module StringBuffer : OBuffer = struct
+  type cur = int
+  type buf = {text:string; cursor:cur}
+
+  let make () = {text=""; cursor=0}
+  
+  let get_text (buf:buf) = buf.text
+  let get_cursor (buf:buf) = buf.cursor
+
+  let set_text (buf:buf) (text:string) = {buf with text=text}
+  let set_cursor (buf:buf) (cursor:cur) = {buf with cursor=cursor}
+
+  let move_cursor_right (buf:buf) =
+    {buf with cursor=buf.cursor + 1}
+  let move_cursor_left (buf:buf) =
+    {buf with cursor=buf.cursor - 1}
+                                  
+  let insert_char (buf:buf) (chr:char) =
+    let left = String.sub buf.text 0 buf.cursor in
+    let right = String.sub buf.text buf.cursor (String.length buf.text - buf.cursor) in
+    let new_text = left ^ (String.make 1 chr) ^ right in
+    {text=new_text; cursor=buf.cursor + 1}
+    
+  let delete_char (buf:buf) =
+    let left = String.sub buf.text 0 (buf.cursor - 1) in
+    let right = String.sub buf.text buf.cursor (String.length buf.text - buf.cursor) in
+    let new_text = left ^ right in
+    {text=new_text; cursor=buf.cursor - 1}
+    
+end
