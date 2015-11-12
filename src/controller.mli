e5b3b2b0c685c98f5549e2a76b298cf15fdfd812
Alex Renda <alex@renda.org>
2015-11-12 16:07:47 -0500

    updated mli, added basic hook framework

--- src/controller.mli
+++ src/controller.mli
@@ -1,11 +1,17 @@
 module type Controller = sig
     type t
     type key
-    type callback = key -> t -> OBuffer.t -> Terminal.t -> unit
+    type hook
+    type api
+    type callback = api -> unit
 
-    val register_keypress_event : callback -> unit
+    val create : unit -> t
+
+    val register_keypress_event : key -> callback -> unit
+    val register_hook : hook -> callback -> unit
 
     val keypress : key -> OBuffer.t -> unit
+    val run_hook : hook -> OBuffer.t -> unit
 
     val eval_file : File.t -> unit
 end
--- src/obuffer.mli
+++ src/obuffer.mli
@@ -11,18 +11,16 @@ module type OBuffer = sig
   val get_mark : t -> pos
 
   (* Basic cursor interaction *)
-  val set_cursor : t -> pos -> unit
-  val move_cursor_right : t -> unit
-  val move_cursor_left : t -> unit
+  val set_cursor : t -> pos -> t
+  val move_cursor_right : t -> t
+  val move_cursor_left : t -> t
 
   (* Basic text interaction *)
-  val set_text : t -> string -> unit
-  val insert_char_at_curor : t -> char -> unit
-  val delete_char_at_cursor : t -> unit
+  val set_text : t -> string -> t
+  val insert_char_at_curor : t -> char -> t
+  val delete_char_at_cursor : t -> t
 
   (* Basic file operations *)
   val write : t -> unit
   val get_file : t -> File.t
 end
-
-module StringBuffer : OBuffer
