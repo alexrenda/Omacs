6b9be6cf15084f2d0fcc882e809aea17c5b90769
Alex Renda <alex@renda.org>
2015-11-12 21:33:54 -0500

    udpates to mli

--- src/interpreter.mli
+++ src/interpreter.mli
@@ -1,4 +1,2 @@
-module type Interpreter = sig
-    val eval_file :
-      File.t -> ('a, 'b) api_function list -> Controller.callback list
-end
+val eval_file :
+  File.t -> ('a, 'b) Controller.api_function list -> unit
