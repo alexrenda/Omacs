80f0c14879fd3f9521dbf1f7765616d2df69860b
Alex Renda <alex@renda.org>
2015-11-12 22:47:52 -0500

    updated file documentation

--- src/file.mli
+++ src/file.mli
@@ -3,7 +3,13 @@ type t
 val file_of_string : string -> t
 
 val get_contents : t -> string
+(* get the name of the file (e.g. "file.mli") *)
 val get_name : t -> string
+(* get the relative path of the file (e.g. "./file.mli") *)
 val get_path : t -> string
 
+(* write the string to the file and flush *)
 val write_string : t -> string -> unit
+
+(* flush and close the file *)
+val close: t -> unit
