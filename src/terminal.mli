d20be52ae9a2a09ac2e37dad84f239b09334089e
Alex Renda <alex@renda.org>
2015-11-12 22:46:23 -0500

    updated terminal.mli documentation

--- src/terminal.mli
+++ src/terminal.mli
@@ -1,7 +1,9 @@
 type t
 
 val create : unit -> t
+(* show this terminal on the terminal. never returns *)
 val show : t -> unit
 val open_file : File.t -> unit
 
+(* nicely close the terminal (possible in response to a C-c event? *)
 val close : t -> unit
