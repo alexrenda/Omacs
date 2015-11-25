c84f0103d42aa832dc2676c989328555fd4fe848
Alex Renda <alex@renda.org>
2015-11-25 02:55:35 -0800

    look upon my types, ye mighty, and despair!

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,32 +1,37 @@
 exception EvalError
 
-let formatter = Format.formatter_of_out_channel (open_out "/dev/null")
+let devnull = open_out "/dev/null"
+let formatter = Format.formatter_of_out_channel devnull
+
+let id_counter = ref 0
+let create_type_for_var name =
+  let open Types in
+  let val_kind = Types.Val_reg in
+  let val_loc = Location.none in
+  let val_attributes = [] in
+
+  let level = Btype.generic_level in
+
+  let id = !id_counter in
+  incr id_counter;
+  let full = {desc = Tarrow ("",
+                             {desc = Tvar (Some "a"); level; id},
+                             {desc = Tvar (Some "b"); level; id},
+                             Cok);
+              level;
+              id}
+  in
+  {Types.val_type=full; val_kind; val_loc; val_attributes}
 
 let write_object name obj =
+  let oldenv = !Toploop.toplevel_env in
+  let t = create_type_for_var name in
+  let newenv = Env.add_value (Ident.create name) t oldenv in
+  Toploop.toplevel_env := newenv;
   Toploop.setvalue name (Obj.repr obj)
 
 let read_object name =
   Obj.obj (Toploop.getvalue name)
-
-(* TODO: CITE THIS *)
-let eval text =
-  try
-    let lexed = Lexing.from_string text in
-    let parsed = !Toploop.parse_toplevel_phrase lexed in
-    ignore(Toploop.execute_phrase false Format.std_formatter parsed)
-  with
-  |Typetexp.Error (loc, env, err) ->
-    let f = Format.formatter_of_out_channel stdout in
-    Typetexp.report_error env f err;
-    print_newline ();
-    raise EvalError
-  | Symtable.Error e ->
-     match e with
-     | Symtable.Undefined_global s
-     | Symtable.Unavailable_primitive s
-     | Symtable.Wrong_vm s
-     | Symtable.Uninitialized_global s -> Printf.printf "err: %s\n" s;
-                                          failwith "asd"
 
 let eval_file file =
   ignore (Toploop.use_file formatter (File.get_path file));
@@ -40,6 +45,8 @@ let _ =
   Toploop.initialize_toplevel_env ();
   let ocamlinit = File.file_of_string ".ocamlinit" in
   let file_text = File.get_contents ocamlinit in
-  eval file_text;
+  let lexed = Lexing.from_string file_text in
+  let parsed = !Toploop.parse_toplevel_phrase lexed in
+  ignore(Toploop.execute_phrase false formatter parsed);
   Topdirs.dir_directory ".";
   Topdirs.dir_directory "_build"
