131905b3fede1cd20c9b572e2bbcc641598205d0
Alex Renda <alex@renda.org>
2015-11-25 02:38:19 -0800

    updated test script + tests to be easier to use, and to include functios, callbacks, etc

--- src/test_interp.ml
+++ src/test_interp.ml
@@ -1,3 +1,14 @@
+let concat_strings a b = a ^ " " ^ b
+let add_ints a b = a - b
+let alpha_to_unit a = Printf.printf "1\n"
+let unit_to_unit () = Printf.printf "2\n"
+
+let _ = Interpreter.register_api_function "concat_strs" concat_strings
+let _ = Interpreter.register_api_function "add_ints" add_ints
+let _ = Interpreter.register_api_function "alpha_to_unit" alpha_to_unit
+let _ = Interpreter.register_api_function "unit_to_unit" unit_to_unit
+
+
 let filename =
   let len = Array.length Sys.argv in
   Sys.argv.(len - 1)
@@ -6,5 +17,9 @@ let controller = Controller.create ()
 let f = File.file_of_string filename
 let c = Controller.eval_file controller f
 let b = OBuffer.make_from_file f
-let _ = Controller.keypress c "x" b
+
+let _ =
+  try
+    ignore (Controller.keypress c (Controller.Char 'x') b)
+  with _ -> ()
 let _ = Printf.printf "we good\n"
--- src/tests/basic.oca.ml
+++ src/tests/basic.oca.ml
@@ -5,6 +5,7 @@ let callback c b =
 
 let register_callbacks (controller:Controller.t) : Controller.t =
   Printf.printf "registering\n%!";
-  let controller = Controller.register_keypress_event controller "x" callback in
+  let controller = Controller.register_keypress_event
+                     controller (Controller.Char 'x') callback in
   controller
 ;;
