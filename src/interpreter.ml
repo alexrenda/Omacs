f8e645a8a1a2d3777e92d94a5733ee489e34b51b
Alex Renda <alex@renda.org>
2015-11-24 20:55:09 -0800

    added slightly better error reporting to the interpreter, made it read .ocamlinit, updated eval_file to be done correctly (enough for now, bad error handling on that), added startup so that interpretation works

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,5 +1,7 @@
 exception EvalError
 
+let formatter = Format.formatter_of_out_channel (open_out "/dev/null")
+
 let write_object name obj =
   Toploop.setvalue name (Obj.repr obj)
 
@@ -12,11 +14,32 @@ let eval text =
     let lexed = Lexing.from_string text in
     let parsed = !Toploop.parse_toplevel_phrase lexed in
     ignore(Toploop.execute_phrase false Format.std_formatter parsed)
-  with _ -> raise EvalError
+  with
+  |Typetexp.Error (loc, env, err) ->
+    let f = Format.formatter_of_out_channel stdout in
+    Typetexp.report_error env f err;
+    print_newline ();
+    raise EvalError
+  | Symtable.Error e ->
+     match e with
+     | Symtable.Undefined_global s
+     | Symtable.Unavailable_primitive s
+     | Symtable.Wrong_vm s
+     | Symtable.Uninitialized_global s -> Printf.printf "err: %s\n" s;
+                                          failwith "asd"
 
-let eval_file file controller =
-  let file_text = File.get_contents file in
-  eval file_text;
+let eval_file file =
+  ignore (Toploop.use_file formatter (File.get_path file));
   read_object "register_callbacks"
 
 let register_api_function = write_object
+
+let _ =
+  Toploop.set_paths ();
+  !Toploop.toplevel_startup_hook ();
+  Toploop.initialize_toplevel_env ();
+  let ocamlinit = File.file_of_string ".ocamlinit" in
+  let file_text = File.get_contents ocamlinit in
+  eval file_text;
+  Topdirs.dir_directory ".";
+  Topdirs.dir_directory "_build"
