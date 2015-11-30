1ab4d9a1da2ccdd731cb6decd66266aec2c33ca3
Alex Renda <alex@renda.org>
2015-11-29 22:51:28 -0500

    added argument support to terminal and run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 cs3110 compile terminal.ml &&
     clear &&
-    cs3110 run terminal
+    cs3110 run terminal $1
--- src/terminal.ml
+++ src/terminal.ml
@@ -2,11 +2,17 @@ open Curses
 
 type t = int
 
+let filename =
+  let len = Array.length Sys.argv in
+  try Sys.argv.(len - 1)
+  with _ -> "*SCRATCH*"
+
 let _ =
-  let buf = OBuffer.make_from_file (File.file_of_string "*SCRATCH*") in
+  let buf = OBuffer.make_from_file (File.file_of_string filename) in
   let window = initscr () in
   let _ = noecho () in
   let _ = (window, buf) in
+  ignore (addstr (OBuffer.get_text buf));
   while true do
     let chr = getch () in
     ignore (addch chr)
