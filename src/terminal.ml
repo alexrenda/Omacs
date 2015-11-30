b59076a2553650f55a2b1ba2d1c67a2ceac9112d
Alex Renda <alex@renda.org>
2015-11-30 13:53:32 -0500

    pretty print!

--- src/terminal.ml
+++ src/terminal.ml
@@ -71,23 +71,54 @@ let draw_line_numbers size ui buf =
   LTerm_draw.draw_styled linum 0 0 (eval [B_fg LTerm_style.lblack;
                                           S linum_str;
                                           E_fg]);
-  text, linum_size.cols
+  text, text_rect
+
+let draw_status size ui buf =
+  let status_size = {size with rows = 2} in
+  let status_rect = {row1=size.rows - status_size.rows; col1=0; col2 = size.cols;
+                    row2 = size.rows} in
+  let rest_rect = {row1=0;
+                   col1=0;
+                   col2 = size.cols;
+                   row2 = status_rect.row1} in
+  let status = LTerm_draw.sub ui status_rect in
+  let rest = LTerm_draw.sub ui rest_rect in
+  let file = OBuffer.get_file buf in
+  let filename = File.get_name file in
+  let f_str = Printf.sprintf "File: %s" filename in
+  let key =
+    match !key_to_print with
+    | None -> ""
+    | Some key -> Utils.string_of_key key
+  in
+  let k_str = Printf.sprintf "Key: %s" key in
+  let status_width = status_size.cols in
+  let status_str = Printf.sprintf "%-*s\n%-*s"
+                                  status_width f_str
+                                  status_width k_str in
+  LTerm_draw.draw_styled status 0 0 (eval [B_fg LTerm_style.lwhite;
+                                           S status_str;
+                                           E_fg]);
+  rest, rest_rect
 
 let draw ui matrix buf =
   let full_size = LTerm_ui.size ui in
-  let ui_size = {full_size with rows = full_size.rows - 2} in
   let ctx = LTerm_draw.context matrix full_size in
-  let ui_rect = {row1=0; col1=0; col2 = ui_size.cols; row2 = ui_size.rows} in
-  let ui_ctx = LTerm_draw.sub ctx ui_rect in
+  let ui_ctx, ui_rect = draw_status full_size ctx !buf in
+  let ui_size = {full_size with rows = ui_rect.row2-ui_rect.row1} in
   LTerm_draw.clear ui_ctx;
 
-  let text, linum_size = draw_line_numbers ui_size ui_ctx buf in
+  let text, text_rect = draw_line_numbers ui_size ui_ctx buf in
+  let linum_size = text_rect.col1 in
 
   buf := OBuffer.set_width !buf (ui_rect.col2 - linum_size);
 
   let row = OBuffer.get_view_row !buf in
-  let str = OBuffer.str_of_buffer !buf in
-  LTerm_draw.draw_string text (-row) 0 str;
+  let text_str = OBuffer.str_of_buffer !buf in
+  (* A9B7C6 *)
+  LTerm_draw.draw_styled text (-row) 0 (eval [B_fg LTerm_style.default;
+                                              S text_str;
+                                              E_fg]);
   LTerm_ui.set_cursor_position ui {row=OBuffer.get_row !buf - row;
                                    col=linum_size + OBuffer.get_col !buf}
 
