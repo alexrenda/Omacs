b71cede09f87b6665e1a7e0cad65c2943e662a92
Mustafa Ansari <maa296@cornell.edu>
2015-12-01 01:22:14 -0500

    Update dotfiles to use core for buffer, and correct files. Fix small bugs so that it all actually builds. Because thats probably important. Priorities.

--- src/oBuffer.ml
+++ src/oBuffer.ml
@@ -1,66 +1,108 @@
 open Core
 open File
 
-type pos = char Doubly_linked.Elt.t option
-type t = {text:char Doubly_linked.t; mutable cursor:pos; mutable mark:pos; file:File.t}
+type pos = int
+type elt = char Doubly_linked.Elt.t option
+type t = {text:char Doubly_linked.t;
+          mutable cursor:(elt*pos);
+          mutable mark:(elt*pos);
+          file:File.t}
 
-let make_from_file file = 
+let elt_of_int lst count =
+    let rec traverse l elt num =
+        match elt with
+        |Some c ->
+            if num = 0 then
+                Some c
+            else
+                traverse l (Doubly_linked.next l c) (num-1)
+        |None -> None
+    in
+    traverse lst (Doubly_linked.first_elt lst) count
+
+
+let make_from_file file cur mar =
     let text = Doubly_linked.create () in
     let cursor = Doubly_linked.insert_first text ' ' in
-    {text=text; cursor=Some cursor; mark=Some cursor; file=file}
+    let start = (Some cursor, 0) in
+    {text=text; cursor=start; mark=start; file=file}
 
 let get_text (buf:t) = 
     let concat accum c = accum^(Char.escaped c) in
     Doubly_linked.fold buf.text ~f:concat ~init:""
-let get_cursor (buf:t) = buf.cursor
-let get_mark (buf:t) = buf.mark
+
+let get_cursor (buf:t) = snd buf.cursor
+
+let get_mark (buf:t) = snd buf.mark
+
 
 let set_cursor (buf:t) (pos:pos) =
-    buf.cursor <- pos;
+    buf.cursor <- (elt_of_int buf.text pos, pos);
     buf
+
 let move_cursor_right (buf:t) =
-    match buf.cursor with
+    match fst buf.cursor with
     | Some c ->
         if not (Doubly_linked.is_last buf.text c) then 
-            buf.cursor <- Doubly_linked.next buf.text c;
+            buf.cursor <- (Doubly_linked.next buf.text c, (snd buf.cursor)+1);
         buf
     | None -> failwith "Bad cursor"
 
 let move_cursor_left (buf:t) =
-    match buf.cursor with
+    match fst buf.cursor with
     | Some c -> 
         if not (Doubly_linked.is_first buf.text c) then 
-            buf.cursor <- Doubly_linked.prev buf.text c;
+            buf.cursor <- (Doubly_linked.prev buf.text c, (snd buf.cursor)-1);
         buf
     | None -> failwith "Bad cursor"
+
 
 let set_text (buf:t) (text:string) = 
     Doubly_linked.clear buf.text;
     String.iter (fun c -> ignore(Doubly_linked.insert_last buf.text c)) (text^" ");
-    let cursor = Doubly_linked.last_elt buf.text in
-    buf.cursor <- cursor;
-    buf.mark <- cursor;
+    let cursor = Doubly_linked.first_elt buf.text in
+    buf.cursor <- (cursor, 0);
+    buf.mark <- (cursor, 0);
     buf
 
 let insert_char_at_cursor (buf:t) (chr:char) =
-  match buf.cursor with 
-  | Some c -> 
-          ignore(Doubly_linked.insert_before buf.text c chr);
-          buf
-  | None -> failwith "Bad cursor"
+    match fst buf.cursor with
+    | Some c ->
+        ignore(Doubly_linked.insert_before buf.text c chr);
+        buf
+    | None -> failwith "Bad cursor"
 
 let delete_char_at_cursor (buf:t) =
-  match buf.cursor with 
-  | Some c -> 
-          if not (Doubly_linked.is_last buf.text c) then ( 
-              buf.cursor <- Doubly_linked.next buf.text c;
-              Doubly_linked.remove buf.text c);
-          buf
-  | None -> failwith "Bad cursor" 
+    match fst buf.cursor with
+    | Some c ->
+        if not (Doubly_linked.is_last buf.text c) then (
+            ignore(move_cursor_right buf);
+            Doubly_linked.remove buf.text c);
+        buf
+    | None -> failwith "Bad cursor"
+
 
 let write (buf:t) =
-  File.write_string buf.file (get_text buf);
-  buf
+    File.write_string buf.file (get_text buf);
+    buf
 
 let get_file (buf:t) = buf.file
+
+
+
+let str_of_buffer (buf:t) = failwith "Unimplemented"
+let set_view_row (buf:t) = failwith "Unimplemented"
+let set_height (buf:t) (height:int) = failwith "Unimplemented"
+let set_width (buf:t) (width:int) = failwith "Unimplemented"
+let set_row (buf:t) (pos:pos) = failwith "Unimplemented"
+let set_col (buf:t) (pos:pos) = failwith "Unimplemented"
+let move_cursor_to_end (buf:t) = failwith "Unimplemented"
+let move_cursor_to_beginning (buf:t) = failwith "Unimplemented"
+let move_cursor_down (buf:t) = failwith "Unimplemented"
+let move_cursor_up (buf:t) = failwith "Unimplemented"
+let get_view_row (buf:t) = failwith "Unimplemented"
+let get_height (buf:t) = failwith "Unimplemented"
+let get_width (buf:t) = failwith "Unimplemented"
+let get_row (buf:t) = failwith "Unimplemented"
+let get_col (buf:t) = failwith "Unimplemented"
 
