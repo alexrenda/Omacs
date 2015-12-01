c8be1a195025237fe0af3bd6e29cdd7e6fbf14c8
Mustafa Ansari <maa296@cornell.edu>
2015-11-30 21:35:34 -0500

    Fix cursor bug with first inserted character

--- src/oBuffer.ml
+++ src/oBuffer.ml
@@ -3,10 +3,11 @@ open Core
 type pos = char Doubly_linked.Elt.t option
 type t = {text:char Doubly_linked.t; cursor:pos; mark:pos; file:File.t}
 
-let make_from_file file = {text=Doubly_linked.create (); 
-                           cursor=None; 
-                           mark=None; 
-                           file=file}
+let make_from_file file = 
+    let text = Doubly_linked.create () in
+    Doubly_linked.insert_first text ' ';
+    let cursor = Doubly_linked.first_elt text in
+    {text=text; cursor=cursor; mark=cursor; file=file}
 
 let get_text (buf:t) = 
     let concat accum c = accum^(Char.escaped c) in
@@ -22,8 +23,8 @@ let move_cursor_right (buf:t) =
           buf
         else
           {buf with cursor=Doubly_linked.next buf.text c}
-    | None -> 
-        {buf with cursor=Doubly_linked.first_elt buf.text}
+    | None -> failwith "Bad cursor"
+
 let move_cursor_left (buf:t) =
     match buf.cursor with
     | Some c -> 
@@ -31,37 +32,31 @@ let move_cursor_left (buf:t) =
           buf
         else
           {buf with cursor=Doubly_linked.prev buf.text c}
-    | None -> 
-        buf
+    | None -> failwith "Bad cursor"
 
 let set_text (buf:t) (text:string) = 
     let temp = Doubly_linked.create () in
-    String.iter (fun c -> ignore(Doubly_linked.insert_last temp c)) text;
-    {buf with text=temp; cursor=None; mark=None}
+    String.iter (fun c -> ignore(Doubly_linked.insert_last temp c)) (text^" ");
+    let cursor = Doubly_linked.last_elt temp in
+    {buf with text=temp; cursor=cursor; mark=cursor}
 
 let insert_char_at_cursor (buf:t) (chr:char) =
   match buf.cursor with 
   | Some c -> 
           ignore(Doubly_linked.insert_before buf.text c chr);
           buf
-  | None -> 
-          ignore(Doubly_linked.insert_first buf.text chr);
-          let cursor' = Doubly_linked.first_elt buf.text in
-          {buf with cursor=cursor'}
+  | None -> failwith "Bad cursor"
 
 let delete_char_at_cursor (buf:t) =
   match buf.cursor with 
   | Some c -> 
           if Doubly_linked.is_last buf.text c then
-              let cursor' = Doubly_linked.prev buf.text c in
-              Doubly_linked.remove buf.text c;
-              {buf with cursor=cursor'}
+              buf
           else
               let cursor' = Doubly_linked.next buf.text c in
               Doubly_linked.remove buf.text c;
               {buf with cursor=cursor'}
-  | None -> 
-          buf
+  | None -> failwith "Bad cursor" 
 
 let write (buf:t) =
   File.write_string buf.file (get_text buf);
