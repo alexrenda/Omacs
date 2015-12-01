2d3ffcf1ccdb99e3cbe2d36e4d3ec79c8d961bf3
Mustafa Ansari <maa296@cornell.edu>
2015-11-30 23:14:46 -0500

    Make buffer mutable

--- src/oBuffer.ml
+++ src/oBuffer.ml
@@ -1,13 +1,13 @@
 open Core
+open File
 
 type pos = char Doubly_linked.Elt.t option
-type t = {text:char Doubly_linked.t; cursor:pos; mark:pos; file:File.t}
+type t = {text:char Doubly_linked.t; mutable cursor:pos; mutable mark:pos; file:File.t}
 
 let make_from_file file = 
     let text = Doubly_linked.create () in
-    Doubly_linked.insert_first text ' ';
-    let cursor = Doubly_linked.first_elt text in
-    {text=text; cursor=cursor; mark=cursor; file=file}
+    let cursor = Doubly_linked.insert_first text ' ' in
+    {text=text; cursor=Some cursor; mark=Some cursor; file=file}
 
 let get_text (buf:t) = 
     let concat accum c = accum^(Char.escaped c) in
@@ -15,30 +15,32 @@ let get_text (buf:t) =
 let get_cursor (buf:t) = buf.cursor
 let get_mark (buf:t) = buf.mark
 
-let set_cursor (buf:t) (pos:pos) = {buf with cursor=pos}
+let set_cursor (buf:t) (pos:pos) =
+    buf.cursor <- pos;
+    buf
 let move_cursor_right (buf:t) =
     match buf.cursor with
     | Some c ->
-        if Doubly_linked.is_last buf.text c then 
-          buf
-        else
-          {buf with cursor=Doubly_linked.next buf.text c}
+        if not (Doubly_linked.is_last buf.text c) then 
+            buf.cursor <- Doubly_linked.next buf.text c;
+        buf
     | None -> failwith "Bad cursor"
 
 let move_cursor_left (buf:t) =
     match buf.cursor with
     | Some c -> 
-        if Doubly_linked.is_first buf.text c then 
-          buf
-        else
-          {buf with cursor=Doubly_linked.prev buf.text c}
+        if not (Doubly_linked.is_first buf.text c) then 
+            buf.cursor <- Doubly_linked.prev buf.text c;
+        buf
     | None -> failwith "Bad cursor"
 
 let set_text (buf:t) (text:string) = 
-    let temp = Doubly_linked.create () in
-    String.iter (fun c -> ignore(Doubly_linked.insert_last temp c)) (text^" ");
-    let cursor = Doubly_linked.last_elt temp in
-    {buf with text=temp; cursor=cursor; mark=cursor}
+    Doubly_linked.clear buf.text;
+    String.iter (fun c -> ignore(Doubly_linked.insert_last buf.text c)) (text^" ");
+    let cursor = Doubly_linked.last_elt buf.text in
+    buf.cursor <- cursor;
+    buf.mark <- cursor;
+    buf
 
 let insert_char_at_cursor (buf:t) (chr:char) =
   match buf.cursor with 
@@ -50,12 +52,10 @@ let insert_char_at_cursor (buf:t) (chr:char) =
 let delete_char_at_cursor (buf:t) =
   match buf.cursor with 
   | Some c -> 
-          if Doubly_linked.is_last buf.text c then
-              buf
-          else
-              let cursor' = Doubly_linked.next buf.text c in
-              Doubly_linked.remove buf.text c;
-              {buf with cursor=cursor'}
+          if not (Doubly_linked.is_last buf.text c) then ( 
+              buf.cursor <- Doubly_linked.next buf.text c;
+              Doubly_linked.remove buf.text c);
+          buf
   | None -> failwith "Bad cursor" 
 
 let write (buf:t) =
