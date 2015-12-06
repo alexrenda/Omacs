55edee449c3a518d1eeb6b6b33030d779eaa2afc
Alex Renda <alex@renda.org>
2015-12-05 20:18:42 -0500

    tracked .oca.ml.d files

---
+++ src/.oca.ml.d/syntax_highlighting.oca.ml
@@ -0,0 +1,24 @@
+let zabb = 1;;
+
+let register_callbacks (controller:Controller.t) : Controller.t =
+  let open Key in
+  let highlight_key = key_of_string "C-s" in
+  let syntax = Str.regexp "syntax" in
+  let style = [Style.ForegroundColor (LTerm_style.rgb 255 0 0)] in
+  let callback c b =
+    let text = OBuffer.get_string b in
+    let search = Str.search_forward syntax text in
+    let rec syntax_highlight_helper idx =
+      try
+        let idx = search idx in
+        OBuffer.set_text_style b idx (idx + 5) style |> ignore;
+        syntax_highlight_helper (idx + 1)
+      with Not_found -> c, b
+    in
+    syntax_highlight_helper 0
+  in
+
+  let controller = Controller.register_keypress_listener
+                     controller highlight_key callback in
+  controller
+;;
